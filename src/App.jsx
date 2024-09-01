686b46080e5c2b41e7c2090b6d8363c238f97613
Francisco López <franlopezsanchez97@gmail.com>
2024-09-01 11:22:13 +0200

    First commit

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  return (
+    <div className='bg-slate-600 grid grid-cols-2 gap-4 min-h-16 min-w-[900px]'>
+      <div className='bg-slate-500 max-w-24'>
+        Image
+      </div>
+      <div className='bg-slate-400 card-textPosition min-w-max'>
+        Cucu
+      </div>
+    </div>
+  )
+}
+
+export default App
