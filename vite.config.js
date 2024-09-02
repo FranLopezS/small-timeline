94645b981cf05056d28506065f332fa89e9d9161
Fran <franlopezsanchez97@gmail.com>
2024-09-02 17:55:36 +0200

    responsive ui

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react-swc'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: 'https://franlopezs.github.io/small-timeline'
+  base: 'small-timeline.pages.dev'
 })
